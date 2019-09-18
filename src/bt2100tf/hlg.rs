7c1488a2994acd64084e1370ff9a421c4bdf6a57
Kazuhei Hamada <hamada.kazuhei@gmail.com>
2019-09-18 22:58:41 +0900

    Change file hierarchy

---
+++ src/bt2100tf/hlg.rs
@@ -0,0 +1,54 @@
+use std::f64;
+
+struct DisplayProp {
+    gamma: f64,
+    peak_luminance: u16,
+    black_luminance: u16,
+}
+
+static display_prop: DisplayProp = DisplayProp {
+    gamma: 1.2,
+    peak_luminance: 1000,
+    black_luminance: 5,
+};
+
+/// There are 3 constants in BT.2100 TABLE 5
+const HLG_A: f64 = 0.17883277;
+const HLG_B: f64 = 1.0 - 4.0 * HLG_A;
+// HLG_C = 0.5 - HLG_A * (4.0 * HLG_A).ln()
+const HLG_C: f64 = 0.5599107;
+
+const Y_R_COEF: f64 = 0.2627;
+const Y_G_COEF: f64 = 0.6780;
+const Y_B_COEF: f64 = 0.0593;
+
+pub fn oetf(color: f64) -> f64 {
+    if color <= 1.0 / 12.0 {
+        (color * 3.0).sqrt()
+    } else {
+        HLG_A * (12.0 * color - HLG_B).ln() + HLG_C
+    }
+}
+
+pub fn inverse_oetf(color: f64) -> f64 {
+    if color <= 1.0 / 2.0 {
+        color * color / 3.0
+    } else {
+        let tmp = (color - HLG_C) / HLG_A;
+        (tmp.exp() + HLG_B) / 12.0
+    }
+}
+
+pub fn ootf(rgb: &mut Vec<f64>) {
+    let y = Y_R_COEF * rgb[0] +
+            Y_G_COEF * rgb[1] +
+            Y_B_COEF * rgb[2];
+    let y_pow = y.powf(display_prop.gamma - 1.0);
+
+    let lw = display_prop.peak_luminance as f64;
+    let lb = display_prop.black_luminance as f64;
+
+    for e in rgb {
+        *e =  (lw - lb) * y_pow * *e + lb;
+    }
+}
